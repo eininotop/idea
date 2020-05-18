78bd30dcd914283ce122a34b867ed5144af7ba81
Your Name <you@example.com>
2020-05-18 22:58:34 +0800

    提交测试代码

--- springtest/src/main/java/top/einino/springtest/SpringtestApplication.java
+++ springtest/src/main/java/top/einino/springtest/SpringtestApplication.java
@@ -8,7 +8,7 @@ public class SpringtestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringtestApplication.class, args);
-        System.out.println("提交数据");
+        System.out.println("提交数据1");
     }
 
 }
